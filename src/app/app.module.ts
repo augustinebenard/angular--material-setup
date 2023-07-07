fb1a96694180504f31515562ad548ef8172d619c
Austin <pheelzconnect@gmail.com>
2023-07-07 20:04:49 +0100

    new angular material demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { ExtrasComponent } from './components/extras/extras.component';
+import { materials } from './angular-material/material-modules';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    ExtrasComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...materials,
+    BrowserAnimationsModule
+  ],
+  exports:[
+    ...materials
   ],
   providers: [],
   bootstrap: [AppComponent]
